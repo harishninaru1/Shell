8dc2970b553ce5cc6295b410d20525c03cfd0af0
harishninaru1 <harishninaru1@gmail.com>
2019-10-01 12:04:06 +0000

    about_variables

---
+++ about_variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+x=1
+y=2
+z=3
+echo "output from double quoations"
+echo "$x"
+echo "$y"
+echo "$z"
+
+echo 'output from single quqtions'
+echo '$x'
+echo '$y'
+echo '$z'
+
+echo output from no quations
+echo $x
+echo $y
+echo $z
